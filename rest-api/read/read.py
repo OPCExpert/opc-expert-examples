11583b59f028267bb464d790a18310de8fe863a2
tomermazor-opcti <tomer.mazor@opcti.com>
2026-08-31 10:41:04 -0600

    Update read.py

--- rest-api/read/read.py
+++ rest-api/read/read.py
@@ -11,16 +11,26 @@ from types import SimpleNamespace
 #   1. Start Command Prompt (cmd.exe)
 #   2. Enter "py -3 -m pip install json"
 
-while(True):
-    response = requests.get("http://desktop-kn9ludo/read?item=opcda://desktop-kn9ludo/ICONICS.SimulatorOPCDA.2/i:Numeric.Memory")
+def Read(items):
+    
+    
+    while(True):
+        response = requests.get(items)
+        
+        #parse response string(JSON) into a JSON object
+        json_object = json.loads(response.text, object_hook=lambda d: SimpleNamespace(**d))
+        data = json_object.data
 
-    #parse response string(JSON) into a JSON object
-    json_object = json.loads(response.text, object_hook=lambda d: SimpleNamespace(**d))
-    data = json_object.data
+        #each item contains the properties: ID, Value, Quality, SourceTimestamp, ServerTimestamp
+        for item in data:
+            print(f"{item.ID} | Value: {item.Properties.Value} StatusCode: {item.Properties.StatusCode} Timestamp: {item.Properties.SourceTimestamp}")
+        
+        #sleep for 1 second
+        time.sleep(1)
 
-    #each item contains the properties: ID, Value, Quality, SourceTimestamp, ServerTimestamp
-    for item in data:
-        print(f"{item.ID} | Value: {item.Properties.Value} StatusCode: {item.Properties.StatusCode} Timestamp: {item.Properties.SourceTimestamp}")
-
-    #sleep for 1 second
-    time.sleep(1)
+if __name__ == "__main__":
+    # For one item:
+    item = "http://desktop-kn9ludo/read?item=opcda://desktop-kn9ludo/ICONICS.SimulatorOPCDA.2/i:Numeric.Memory"
+    # For two items or more:
+    multipleItems = "http://desktop-kn9ludo/read?item=opcda://desktop-kn9ludo/ICONICS.SimulatorOPCDA.2/i:Numeric.Memory&item=opcda://desktop-kn9ludo/ICONICS.SimulatorOPCDA.2/i:Numeric.Ramp"
+    Read(multipleItems)
